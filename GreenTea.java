ef0e2c51b7d952c33b4aabfef246222e942da163
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-03-18 10:44:53 +0530

    Create GreenTea.java

---
+++ GreenTea.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class GreenTea extends Tea{
+    @Override
+    public void prepareTea() {
+        System.out.println("Preparing green tea with hot water and green tea leaves.");
+    }
+}
