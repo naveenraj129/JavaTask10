2aa4fc51b287a3b50b4f4f7cbeaabb9de03e2d44
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-03-18 10:46:01 +0530

    Create Tea.java

---
+++ Tea.java
@@ -0,0 +1,17 @@
+package org.example;
+
+    public class Tea {
+        public void prepareTea() {
+            System.out.println("Preparing basic tea with hot water and tea leaves.");
+        }
+
+        public void addMilk() {
+            System.out.println("Adding milk to the basic tea.");
+        }
+
+        public void addSugar() {
+            System.out.println("Adding sugar to the basic tea.");
+        }
+    }
+
+
