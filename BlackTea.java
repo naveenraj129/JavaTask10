839bff3910463f1f7945a2f0949a351c52c053e8
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-03-18 10:43:44 +0530

    Create BlackTea.java

---
+++ BlackTea.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class BlackTea extends Tea {
+
+    @Override
+    public void prepareTea() {
+        System.out.println("Preparing black tea with hot water and black tea leaves.");
+    }
+
+}
+
+
