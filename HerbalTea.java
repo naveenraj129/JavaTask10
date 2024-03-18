1d0f651a52b791d30f72a464b4572e8afbd7d45b
naveenraj129 <153100819+naveenraj129@users.noreply.github.com>
2024-03-18 10:45:09 +0530

    Create HerbalTea.java

---
+++ HerbalTea.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class HerbalTea extends Tea {
+    @Override
+    public void prepareTea() {
+        System.out.println("Preparing herbal tea with hot water and herbal tea leaves.");
+    }
+}
+
+
